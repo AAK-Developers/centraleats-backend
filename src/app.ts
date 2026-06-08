1aabd13d01f181345f9f20b33050c177ab11d523
AntonyC12 <coelloantony1212@gmail.com>
2026-06-08 13:57:56 -0500

    feat(app): register /api/protected-test route

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ import { env } from "./config/env";
 import { createOrdersModuleRouter } from "./modules/orders";
 import { createCatalogModuleRouter } from "./modules/catalog";
 import { createVendorModuleRouter } from "./modules/vendors";
+import { requireAuth } from "./shared/middlewares/requireAuth";
+import { protectedTestRoutes } from "./modules/auth";
 import { errorHandler } from "./shared/middlewares/errorHandler";
 
 export const createApp = () => {
@@ -24,6 +26,7 @@ export const createApp = () => {
   app.use("/api/orders", createOrdersModuleRouter());
   app.use("/api", createCatalogModuleRouter());
   app.use("/api/vendors", createVendorModuleRouter());
+  app.use("/api/protected-test", requireAuth, protectedTestRoutes);
 
   app.use(errorHandler);
 
